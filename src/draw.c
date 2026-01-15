1a46a8e4a50241e1e978beb319f418b407c87a47
Christian Dähne <christiandaehne@gmx.net>
2026-01-15 16:07:16 +0100

    Take draw.c from dev

--- src/draw.c
+++ src/draw.c
@@ -3,53 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   draw.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fschnorr <fschnorr@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: vboxuser <vboxuser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 12:36:29 by fschnorr          #+#    #+#             */
-/*   Updated: 2025/11/06 15:02:24 by fschnorr         ###   ########.fr       */
+/*   Updated: 2026/01/15 14:14:07 by vboxuser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D.h"
+#include <stdio.h>
 
-void	draw_testline(t_vars *vars)
+void	draw_fov(t_vars *vars)
 {
-	int		i;
-	float	ray_x;
-	float	ray_y;
-	float	cos_angle;
-	float	sin_angle;
+	float	rad_per_pixel;
+	float	fov_min;
+	int		ray_id;
 
-	ray_x = vars->player.x;
-	ray_y = vars->player.y;
-	cos_angle = cos(vars->player.angle);
-	sin_angle = -sin(vars->player.angle);
-	i = 0;
-	while (i < 100)
+	rad_per_pixel = PI / 3 / WIDTH;
+	fov_min = vars->player.alpha - PI / 6;
+	ray_id = 0;
+	while (ray_id < WIDTH)
 	{
-		put_pixel(vars, ray_x, ray_y, 0xFF0000);
-		ray_x += cos_angle;
-		ray_y += sin_angle;
-		i++;
-	}
-}
-
-void	draw_map(t_vars *vars)
-{
-	int	y;
-	int	x;
-
-	y = 0;
-	while (vars->map.grid[y])
-	{
-		x = 0;
-		while (vars->map.grid[y][x])
-		{
-			if (vars->map.grid[y][x] == '1')
-				draw_square(vars, x * BLOCK, y * BLOCK, BLOCK, 0x0000FF);
-			x++;
-		}
-		y++;
+		cast_ray(vars, fov_min, ray_id, true);
+		fov_min += rad_per_pixel;
+		ray_id++;
 	}
 }
 
@@ -75,41 +52,12 @@ int	draw_img(t_vars *vars)
 {
 	move_player(vars);
 	clear_image(vars);
+	printf("NO texture: %p\n", vars->map.no.img);
+	printf("floor %d,%d,%d\n", vars->map.floor[0], vars->map.floor[1], vars->map.floor[2]);
+	draw_fov(vars);
 	draw_map(vars);
-	draw_square(vars, vars->player.x, vars->player.y, 10, 0x00FF00);
-	draw_testline(vars);
 	mlx_put_image_to_window(vars->mlx, vars->win, vars->img, 0, 0);
+	//wall_detector_helper(vars);
 	return (0);
-}
-
-void	put_pixel(t_vars *vars, int x, int y, int color)
-{
-	int	index;
-
-	if (x < 0 || y < 0 || x >= WIDTH || y >= HEIGHT)
-		return ;
-	index = y * vars->size_line + x * vars->bpp / 8;
-	vars->img_addr[index] = color & 0xFF;
-	vars->img_addr[index + 1] = (color >> 8) & 0xFF;
-	vars->img_addr[index + 2] = (color >> 16) & 0xFF;
-}
-
-
-void	draw_square(t_vars *vars, int x, int y, int size, int color)
-{
-	int	i;
-
-	i = -1;
-	while (++i < size)
-		put_pixel(vars, x + i, y, color);
-	i = -1;
-	while (++i < size)
-		put_pixel(vars, x, y + i, color);
-	i = -1;
-	while (++i < size)
-		put_pixel(vars, x + size, y + i, color);
-	i = -1;
-	while (++i < size)
-		put_pixel(vars, x + i, y + size, color);
 }
 
