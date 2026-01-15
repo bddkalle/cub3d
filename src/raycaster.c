5a74f9c91efdd76d2b2320a27e798afed46b8419
Christian Dähne <christiandaehne@gmx.net>
2026-01-15 18:12:38 +0100

    unified wall touches through wall_info function

--- src/raycaster.c
+++ src/raycaster.c
@@ -23,39 +23,40 @@ t_wall_orient	wall_orient(t_vars *vars, float px, float py)
 
 /* wall orientation is determined by rounding down ray coordinates when grid=1 is touched.
 For edge cases it is necessary to check wether angle is left/right or up/down */
-t_touch	wall_info(t_vars *vars, float px, float py, float beta)
+void	wall_orientation(t_touch *wall_slice, float px, float py, float beta)
 {
-	t_touch	wall_slice;
-
 	if ((int)px % BLOCK == 0 && cos(beta) > 0)
 	{
-		wall_slice.wall_orient = WEST;
-		wall_slice.offset = (int)py;
+		wall_slice->wall_orient = WEST;
+		wall_slice->offset = (int)py;
 	}
 	if ((int)px % BLOCK == BLOCK - 1 && cos(beta) < 0)
 	{
-		wall_slice.wall_orient = EAST;
-		wall_slice.offset = BLOCK - (int)py;
+		wall_slice->wall_orient = EAST;
+		wall_slice->offset = BLOCK - (int)py;
 	}
 	if ((int)py % BLOCK == 0 && sin(beta) > 0)
 	{
-		wall_slice.wall_orient = NORTH;
-		wall_slice.offset = (int)px;
+		wall_slice->wall_orient = NORTH;
+		wall_slice->offset = (int)px;
 	}
 	if ((int)py % BLOCK == BLOCK - 1 && sin(beta) < 0)
 	{
-		wall_slice.wall_orient = SOUTH;
-		wall_slice.offset = (int)py;
+		wall_slice->wall_orient = SOUTH;
+		wall_slice->offset = (int)py;
 	}
+}
 
-	wall_slice.distance = correct_distance(
+void	wall_info(t_vars *vars, t_touch *wall_slice, float px, float py, float beta)
+{
+	wall_orientation(wall_slice, px, py, beta);
+	wall_slice->distance = correct_distance(
 			vars,\
 			distance(\
 				px - vars->player.x,\
 				py - vars->player.y),\
 			beta);
-	wall_projection(&wall_slice);
-	return (wall_slice);
+	wall_projection(wall_slice);
 }
 
 bool	touch(t_vars *vars, float px, float py)
@@ -71,7 +72,7 @@ bool	touch(t_vars *vars, float px, float py)
 		return (true);
 }
 
-void	draw_vertical_line(t_vars *vars, int ray_id, float dist, t_touch *touch)
+void	draw_vertical_line(t_vars *vars, int ray_id, t_touch *touch)
 {
 	int		y;
 
@@ -90,7 +91,6 @@ void	cast_ray(t_vars *vars, float beta, int ray_id, bool draw_map)
 {
 	float	ray_x;
 	float	ray_y;
-	float	dist;
 	t_touch	wall_slice;
 
 	ray_x = vars->player.x;
@@ -102,8 +102,9 @@ void	cast_ray(t_vars *vars, float beta, int ray_id, bool draw_map)
 		ray_x += cos(beta);
 		ray_y += sin(beta);
 	}
-
-	wall_slice = wall_info(vars, ray_x, ray_y, beta);
+	//wall_slice = malloc(sizeof(t_touch));
+	wall_info(vars, &wall_slice, ray_x, ray_y, beta);
 	//draw_vertical_line(vars, ray_x, ray_y, ray_id, beta);
-	draw_vertical_line(vars, ray_id, dist, &wall_slice);
+	draw_vertical_line(vars, ray_id, &wall_slice);
+	//free(wall_slice);
 }
