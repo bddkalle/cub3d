daacedb2d83d8600777a7ec608d7dc65623809ef
Christian Dähne <christiandaehne@gmx.net>
2026-01-23 09:33:15 +0100

    fixed corner texture issue

--- src/raycaster2.c
+++ src/raycaster2.c
@@ -119,10 +119,80 @@ void	wall_orientation2(t_vars *vars, t_touch *wall_slice, float beta)
 		wall_slice->txt = NULL;
 }
 
+void	decide_so_we(t_vars *vars, t_touch *wall_slice)
+{
+	if ((int)(wall_slice->touchpoint.y + 1) % BLOCK == 0 && touch(vars, wall_slice->touchpoint.x, wall_slice->touchpoint.y + 1) == NOTOUCH)
+	{
+		wall_slice->txt = &vars->map.so;
+		wall_slice->offset = (int)wall_slice->touchpoint.x % BLOCK; // handle mirroring
+	}
+	else
+	{
+		wall_slice->txt = &vars->map.we;
+		wall_slice->offset = BLOCK - 1 - (int)wall_slice->touchpoint.y % BLOCK;
+	}
+}
+
+void	decide_so_ea(t_vars *vars, t_touch *wall_slice)
+{
+	if ((int)(wall_slice->touchpoint.y + 1) % BLOCK == 0 && touch(vars, wall_slice->touchpoint.x, wall_slice->touchpoint.y + 1) == NOTOUCH)
+	{
+		wall_slice->txt = &vars->map.so;
+		wall_slice->offset = (int)wall_slice->touchpoint.x % BLOCK; // handle mirroring
+	}
+	else
+	{
+		wall_slice->txt = &vars->map.ea;
+		wall_slice->offset = (int)wall_slice->touchpoint.y % BLOCK;
+	}
+}
+
+void	decide_no_we(t_vars *vars, t_touch *wall_slice)
+{
+	if ((int)(wall_slice->touchpoint.y) % BLOCK == 0 && touch(vars, wall_slice->touchpoint.x, wall_slice->touchpoint.y - 1) == NOTOUCH)
+	{
+		wall_slice->txt = &vars->map.no;
+		wall_slice->offset = BLOCK - 1 - (int)wall_slice->touchpoint.x % BLOCK; // handle mirroring
+	}
+	else
+	{
+		wall_slice->txt = &vars->map.we;
+		wall_slice->offset = BLOCK - 1 - (int)wall_slice->touchpoint.y % BLOCK;
+	}
+}
+
+void	decide_no_ea(t_vars *vars, t_touch *wall_slice)
+{
+	if ((int)(wall_slice->touchpoint.y) % BLOCK == 0 && touch(vars, wall_slice->touchpoint.x, wall_slice->touchpoint.y - 1) == NOTOUCH)
+	{
+		wall_slice->txt = &vars->map.no;
+		wall_slice->offset = BLOCK - 1 - (int)wall_slice->touchpoint.x % BLOCK; // handle mirroring
+	}
+	else
+	{
+		wall_slice->txt = &vars->map.ea;
+		wall_slice->offset = (int)wall_slice->touchpoint.y % BLOCK;
+	}
+}
+
+void	wall_orientation3(t_vars *vars, t_touch *wall_slice, float beta)
+{
+	(void)beta;
+	if (vars->player.y > wall_slice->touchpoint.y && vars->player.x > wall_slice->touchpoint.x)
+		decide_so_we(vars, wall_slice);
+	else if (vars->player.y > wall_slice->touchpoint.y && vars->player.x < wall_slice->touchpoint.x)
+		decide_so_ea(vars, wall_slice);
+	else if (vars->player.y <= wall_slice->touchpoint.y && vars->player.x >= wall_slice->touchpoint.x)
+		decide_no_we(vars, wall_slice);
+	else if (vars->player.y <= wall_slice->touchpoint.y && vars->player.x <= wall_slice->touchpoint.x)
+		decide_no_ea(vars, wall_slice);
+}
+
+
 void	wall_info2(t_vars *vars, t_touch *wall_slice, float beta)
 {
 	wall_slice->distance = correct_distance(vars, wall_slice->distance, beta);
-	wall_orientation2(vars, wall_slice, beta);
+	wall_orientation3(vars, wall_slice, beta);
 	wall_projection(wall_slice);
 }
 
